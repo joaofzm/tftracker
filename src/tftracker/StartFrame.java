3e506c2e1d9275825554a79e169ea137fd8c6418
joaofzm <joao.fzm@hotmail.com>
2021-03-21 02:35:59 -0300

    Update StartFrame.java

--- src/tftracker/StartFrame.java
+++ src/tftracker/StartFrame.java
@@ -10,6 +10,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.border.EmptyBorder;
 
 	public class StartFrame extends JFrame implements ActionListener {
 
@@ -18,27 +19,60 @@ import javax.swing.JLabel;
 		JButton exitButton;
 		static Gui gui;
 
+		JLabel label;
+		JLabel example1;
+		JLabel example2;
+
 		public StartFrame() {
 
+			example1 = new JLabel();
+			ImageIcon example1Image = new ImageIcon(StartFrame.class.getResource("example1.png"));
+			example1.setIcon(example1Image);
+			example1.setBounds(285, 530, 250, 226);
+
+			example2 = new JLabel();
+			ImageIcon example2Image = new ImageIcon(StartFrame.class.getResource("example2.png"));
+			example2.setIcon(example2Image);
+			example2.setBounds(535, 530, 80, 226);
+
+			label = new JLabel();
+			label.setBounds(0, 110, 900, 450);
+			label.setBorder(new EmptyBorder(20, 20, 20, 20));
+			label.setText(
+					"<html><body><br>After you click ''start'', click each of the arenas and fill them up with your opponents "
+							+ "names, remember to LEAVE YOURSELF BLANK!<br><br>"
+							+ " You can fill the arenas during the loading screen or after the game started, just follow "
+							+ "the order in the images bellow:<br><br>"
+							+ "After naming all the opponents, and leaving yourself blank, just click the opponent you fight each"
+							+ " round, and the app will keep track of your possible next opponents, red squares are your possible opponents. <br><br>"
+							+ "When a player is eliminated, click the ''eliminate'' button and eliminate that player, the algorithm will reset, and"
+							+ "you can keep using the tracker the same way."
+					+ "</body></html>");
+			label.setFont(new Font("Arial", Font.BOLD, 22));
+			label.setVerticalTextPosition(JLabel.NORTH);
+			label.setHorizontalTextPosition(JLabel.CENTER);
+			label.setForeground(Color.black);
+			label.setFocusable(false);
+
 			startButton = new JButton();
-			startButton.setBounds(300, 675, 300, 100);
+			startButton.setBounds(300, 765, 300, 50);
 			startButton.addActionListener(this);
 			startButton.setText("S T A R T");
 			startButton.setFont(new Font("Impact", Font.BOLD, 22));
 			startButton.setForeground(Color.white);
-			startButton.setBackground(new Color(0x1E710E));
+			startButton.setBackground(new Color(0, 28, 255));
 			startButton.setFocusable(false);
 
 			exitButton = new JButton();
-			exitButton.setBounds(375, 800, 150, 50);
+			exitButton.setBounds(375, 830, 150, 50);
 			exitButton.addActionListener(this);
 			exitButton.setText("E X I T");
 			exitButton.setFont(new Font("Impact", Font.BOLD, 22));
 			exitButton.setForeground(Color.white);
-			exitButton.setBackground(new Color(0x982f30));
+			exitButton.setBackground(Color.red);
 			exitButton.setFocusable(false);
 
-			this.setContentPane(new JLabel(new ImageIcon(getClass().getClassLoader().getResource(""))));
+			this.setContentPane(new JLabel(new ImageIcon(getClass().getClassLoader().getResource("background.jpg"))));
 			this.setBackground(new Color(100, 65, 165));
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			this.setLayout(null);
@@ -53,6 +87,9 @@ import javax.swing.JLabel;
 
 			this.add(startButton);
 			this.add(exitButton);
+			this.add(label);
+			this.add(example1);
+			this.add(example2);
 
 		}
 
